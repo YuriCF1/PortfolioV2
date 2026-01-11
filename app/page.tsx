f45ccacca4bc700c056594220ba195f736e35b3a
Yuri Cruz <yuricruzf@gmail.com>
2026-01-10 23:54:19 -0300

    Improving styles components structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Header from "@/components/Header";
-import About from "@/components/About";
+import Home from "@/components/Home";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <Header />
       <main className="pt-20 sm:pt-24 md:pt-28">
-        <About />
+        <Home />
       </main>
     </>
   );
--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,9 +1,24 @@
 // import styles from './about.module.css'
 
-const About = () => {
+import { HiArrowRight } from "react-icons/hi";
+
+
+const Home = () => {
     return (
         <div className="px-10 lg:px-35 flex flex-col gap-4 sm:gap-6">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-[1.3] sm:leading-[1.35] md:leading-[1.4] bg-[linear-gradient(45deg,#000_1%,#9ca3af_35%,#e5e7eb_80%)] bg-clip-text text-transparent">
+            <h1 className="
+  text-3xl sm:text-4xl md:text-5xl lg:text-6xl
+  font-bold
+  leading-[1.3] sm:leading-[1.35] md:leading-[1.4]
+
+  /* MOBILE */
+  bg-[linear-gradient(45deg,#000_10%,#6b7280_45%,#9ca3af_70%)]
+
+  /* DESKTOP */
+  sm:bg-[linear-gradient(45deg,#000_1%,#9ca3af_35%,#e5e7eb_80%)]
+
+  bg-clip-text text-transparent
+">
                 Yuri Cruz
                 <br />
                 <span className="block mt-1 text-xl sm:text-2xl md:text-3xl bg-[linear-gradient(45deg,#030a12_0%,#bdbdbd_65%)] sm:bg-[linear-gradient(45deg,#030a12_0%,#575f69_70%,#bdbdbd_85%)] md:bg-[linear-gradient(45deg,#030a12_10%,#575f69_20%,#bdbdbd_80%)] bg-clip-text text-transparent">
@@ -17,19 +32,40 @@ const About = () => {
                 Building products for all types of users and providing good experiences is what I enjoy the most.
             </p>
             <div className="mt-6 flex gap-4">
-                <div className="flex px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700 transition-colors">
+                <div className="group flex px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700 transition-colors">
                     <a href="#projects" className="flex items-center gap-1">
                         My Projects
-                        <span className="text-xs">→</span>
+                        <HiArrowRight
+                            className="
+          text-xl text-white
+          transition-transform
+          group-hover:translate-x-1
+        "
+                        />
                     </a>
                 </div>
-                <a href="#contact" className="px-4 py-2 border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-50 transition-colors flex items-center gap-1">
+
+                <a
+                    href="#contact"
+                    className="
+      group px-4 py-2 border border-gray-300 text-gray-700 rounded-lg
+      hover:bg-gray-50 transition-colors
+      flex items-center gap-1
+    "
+                >
                     Get In Touch
-                    <span className="text-xs">→</span>
+                    <HiArrowRight
+                        className="
+        text-xl text-gray-400
+        transition-transform
+        group-hover:translate-x-1
+      "
+                    />
                 </a>
             </div>
+
         </div>
     )
 }
 
-export default About
+export default Home
